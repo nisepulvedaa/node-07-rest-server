618a48506ef1f8e5dbce31da33926f3bb01195d0
Nicolas Sepulveda Alvear <ni.sepulvedaa@gmail.com>
2019-07-19 01:24:40 -0400

    Primer Commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('./config/config');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuarios');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuarios');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el Puerto:', process.env.PORT);
+});
