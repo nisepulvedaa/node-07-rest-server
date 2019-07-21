3684817404151590f19634c442c73a3115ca90a7
Nicolas Sepulveda Alvear <ni.sepulvedaa@gmail.com>
2019-07-21 12:30:08 -0400

    Goole Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,17 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
 process.env.SEED = process.env.REST_SEED || 'este-es-el-seed-desarrollo';
 
+//
+// google CLIENT_ID
+//
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '496200867736-t2huesdcmhf2fmp6te44asb519j1al8p.apps.googleusercontent.com';
+
+
 let urlDB;
+
+
+
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 require('./config/config');
 
 
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+
+//habilitar carpeta public 
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Configuración 
 app.use(require('./routes/index'));
